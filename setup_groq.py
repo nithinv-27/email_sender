6d328c57ae6bfa02f19b30e1b89c3b22843fd850
Taraka Nithin Vankala <nithintaraka.v@gmail.com>
2024-11-15 23:01:12 +0530

    setup_groq

---
+++ setup_groq.py
@@ -0,0 +1,15 @@
+import os
+import getpass
+from langchain_groq import ChatGroq
+
+if "GROQ_API_KEY" not in os.environ:
+    os.environ["GROQ_API_KEY"] = "Your API Key"
+
+
+llm = ChatGroq(
+    model="mixtral-8x7b-32768",
+    temperature=0.0,
+    max_retries=2,
+    # other params...
+)
+
